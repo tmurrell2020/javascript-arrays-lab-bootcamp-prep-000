30ecca982caa41ede04740220bb59f08ad0ed656
Terrell Murrell <littlerell12@gmail.com>
2020-07-22 17:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-var kittens = [] //define your array here
-
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
+function destructivelyAppendKitten() {
+  kittens.push('Ralph');
+  return kittens;
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift('Bob');
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+
+}
